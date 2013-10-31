256fac00ec1ca023806657beaded75f8edfd3a94
Robert Sun <sunting.bcwl@gmail.com>
2013-10-31 18:57:29 +0800

    add some arm assemble demo

---
+++ demo/README.md
@@ -0,0 +1,3 @@
+#assemble-language-study
+
+This is the code snippets when I learning assemble language programming. Just useless.
---
+++ demo/gnu-hello.s
@@ -0,0 +1,17 @@
+.text
+.globl _start
+_start:
+        movl    $len, %edx
+        movl    $msg, %ecx
+        movl    $1, %ebx
+        movl    $4, %eax
+        int     $0x80
+
+        movl    $0, %ebx
+        movl    $1, %eax
+        int     $0x80
+
+.data
+msg:
+        .ascii  "Hello, world!\n"
+        len =   . - msg
